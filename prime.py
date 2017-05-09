47a1c87cf17ce740a3938e5d82095f1030bbd216
shrikam <skambi@gmail.com>
2017-05-09 19:45:31 +0530

    f

--- prime.py
+++ prime.py
@@ -1,9 +1,10 @@
-n = int(input("entire the number : "))
+n = int(input("enter the number : "))
 
-# divisible by itself or 1
+# Only divisible by itself or 1
 
 if n>1 :
     for i in range(2,n):
+        print (i)
         if n%i==0 :
             print("not a prime")
             break
